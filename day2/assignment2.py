23cd672aefc41976d13b96f0fccd89d89c989f07
mrwilsun <yorickwilson@hotmail.com>
2019-07-16 17:07:17 +0000

    This is the assignment for day 2, done during the lab time. It was fun. Will try to do the question 2 at home although we were asked not to

---
+++ day2/assignment2.py
@@ -0,0 +1,23 @@
+print ('"There is no difference between the greek and the jew."')
+
+print("This \', is an apostrophe")
+
+print("Let us \n move to the next line.")
+
+print("No \
+multiline here")
+
+message = "I am groot!"
+message_lenght = len(message)
+
+print("Message is %d characters long" %message_lenght)
+
+response = "I am also Yorick"
+
+print(message+response)
+
+conversation = message + "," + response
+print(conversation)
+
+new_word ="bazinga"
+print(new_word[2:-1])
