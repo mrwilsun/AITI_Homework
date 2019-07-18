3b849e03a11e9a246e64453038be7ed05583f59c
mrwilsun <yorickwilson@hotmail.com>
2019-07-18 16:58:56 +0000

    converting images into ascii images

---
+++ day4/degreeConverter.py
@@ -0,0 +1,6 @@
+def cel_to_fahr(temp):
+    fahr=temp*1.8 +32.0
+    return fahr
+
+answer=cel_to_fahr(22)
+print (answer)
---
+++ day4/functions.py
@@ -0,0 +1,34 @@
+def grade(mark):
+    if mark >=80:
+        print("A")
+    elif mark >=65:
+        print("B")
+    elif mark >=55:
+        print("C")
+    else:
+        print("E")
+
+def grade2():
+    mark=input("Hello, please enter your mark: ")
+    mark=int(mark)
+    if mark >=80:
+            print("A")
+    elif mark >=65:
+        print("B")
+    elif mark >=55:
+        print("C")
+    else:
+        print("E")
+
+         
+while input("Type x to exit or type any other key to continue >>>")!="x":
+        mark=input("Hello, please enter your mark: ")
+        mark=int(mark)
+        if mark >=80:
+                print("A")
+        elif mark >=65:
+            print("B")
+        elif mark >=55:
+            print("C")
+        else:
+            print("E")
