0eaea961a933dd68e02a288c3c22668c19033eba
mrwilsun <yorickwilson@hotmail.com>
2019-07-30 15:33:04 +0000

    added functions under various classes

--- class_exams/car_wash.py
+++ class_exams/car_wash.py
@@ -1,3 +1,13 @@
-import datetime
+import customer
+import car
 
-print(datetime.datetime.now().time()+1)
+Customer1=customer.Customer("Yorick","Wilson","yw@gmail.com","0244419419","@hello_world")
+
+Car1=car.Car("GR-0555-1516",Customer1)
+print(Customer1.first_name)
+print(Car1.__dict__)
+Car1.show_car_info()
+# print(Car1.lincense_num)
+
+# get_customer_info(customer)
+#     print ("Customer info")
--- class_exams/customer.py
+++ class_exams/customer.py
@@ -2,17 +2,27 @@ from datetime import datetime
 from datetime import timedelta
 
 class Customer:
-    def __init__(self,first_name,last_name,email,phone_number,twitter_handle,cars):
+    def __init__(self,first_name,last_name,email,phone_number,twitter_handle):
         self.first_name=first_name
         self.last_name=last_name
         self.email=email
         self.phone_number=phone_number
         self.twitter_handle=twitter_handle
-        self.cars=cars
+        self.cars_list=[]
 
     def wash_car(car):
         now = datetime.datetime.now()
         date=now.strftime("%d-%m-%Y")
         time_in=datetime.now.time()
         time_done=time_in+timedelta(minutes=20)
+        alert_time=time_done+timedelta(hours=1)
+        add_car(customer,car)
+
+    def add_car(self,car):
+        for i in self.cars_list:
+            if i==car:
+                break
+                self.cars_list.append(car)
         
+    def show_time(self):
+        print("the alert time is" self.alert_time)
