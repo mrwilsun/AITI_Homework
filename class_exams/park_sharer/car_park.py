48b8645dabccd21d11dcb996670c46f6425c0294
mrwilsun <yorickwilson@hotmail.com>
2019-07-31 21:19:02 +0000

    park slot app

---
+++ class_exams/park_sharer/car_park.py
@@ -0,0 +1,12 @@
+class Car_park:
+    def __init__(self,location,capacity,vacancy,parked_cars):
+        self.location=location
+        self.capacity=capacity
+        self.vacancy=vacancy
+        self.parked_cars=parked_cars
+
+
+
+class Parking_space(Car_park):
+    def __init__()
+        
